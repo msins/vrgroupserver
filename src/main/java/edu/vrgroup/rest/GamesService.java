b25498247cae6666e605f59765c1b10c6ddcc8dc
Marko Sinkovic <marko.sinkovic@fer.hr>
2019-12-03 14:15:48 +0100

    Fixed rest.

--- src/main/java/edu/vrgroup/rest/GamesService.java
+++ src/main/java/edu/vrgroup/rest/GamesService.java
@@ -1,5 +1,13 @@
 package edu.vrgroup.rest;
 
+import com.google.gson.annotations.Expose;
+import edu.vrgroup.database.DaoProvider;
+import edu.vrgroup.model.Game;
+import edu.vrgroup.model.Question;
+import edu.vrgroup.model.Scenario;
+import edu.vrgroup.util.JsonUtils;
+import java.util.List;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,7 +22,38 @@ public class GamesService {
   @Path("{game}")
   @Produces(MediaType.APPLICATION_JSON)
   public Response getGameInformation(@PathParam("game") String game) {
-    String output = "{\n  \"game\": " + game + "\n}";
+    GameResponse response = new GameResponse(new Game(game));
+
+    if (!response.gameExists()) {
+      return Response.status(404).entity("There is no game with that name.").build();
+    }
+
+    response.get();
+
+    String output = JsonUtils.toJson(response);
     return Response.status(200).entity(output).build();
   }
+
+  private static class GameResponse {
+
+    @Expose
+    private List<Scenario> scenarios;
+    @Expose
+    private List<Question> questions;
+    private Game game;
+
+    public GameResponse(Game game) {
+      this.game = game;
+    }
+
+    public boolean gameExists() {
+      return DaoProvider.getDao().containsGame(game);
+    }
+
+    public void get() {
+      questions = DaoProvider.getDao().getQuestions(game);
+      scenarios = List.of(Scenario.DEFAULT);
+    }
+  }
+
 }
