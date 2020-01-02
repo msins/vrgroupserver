8639cfd2c52ef2111bd54d836863978e6c96f2bb
msins <m.sinko2@hotmail.com>
2020-01-02 15:53:33 +0100

    Rollback vaadin version, set db timeout to 30 days

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-directory=$1
 cd ~
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs
-sudo apt update
 sudo apt-get install openjdk-11-jdk
 sudo apt-get install maven
 
-GREEN=`tput setaf 10`
 sudo apt install mysql-server
 sudo mysql_secure_installation
 read -p 'Username (used for login): ' username
 read -sp 'Password (used for login): ' password
-mysql -u root -e "create database vrserver; GRANT ALL PRIVILEGES ON vrserver.* to '$username'@localhost IDENTIFIED BY '$password';"
-mysql -h localhost -u '$username' --password='$password' vrserver < ~/vrgroupserver/setup/init.sql
+sudo mysql -e "create user '$username'@'localhost' identified by '$password'"
+sudo mysql -e "create database vrserver; GRANT ALL PRIVILEGES ON vrserver.* to '$username'@'localhost'"
+sudo mysql -e "use vrserver;source $HOME/server/setup/init.sql;"
+sudo mysql -e "SET GLOBAL wait_timeout=2592000"
