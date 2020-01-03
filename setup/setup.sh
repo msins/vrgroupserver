5b887351bd9d1015fb1679735d5496fbedc0e600
msins <m.sinko2@hotmail.com>
2020-01-03 20:56:40 +0100

    Fix setting mysql timeout

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd ~
+sudo apt-get update
 sudo apt-get install openjdk-11-jdk
 sudo apt-get install maven
 
@@ -9,5 +10,8 @@ read -p 'Username (used for login): ' username
 read -sp 'Password (used for login): ' password
 sudo mysql -e "create user '$username'@'localhost' identified by '$password'"
 sudo mysql -e "create database vrserver; GRANT ALL PRIVILEGES ON vrserver.* to '$username'@'localhost'"
-sudo mysql -e "use vrserver;source $HOME/server/setup/init.sql;"
-sudo mysql -e "SET GLOBAL wait_timeout=2592000"
+sudo mysql -e "use vrserver;source $HOME/vrgroupserver/setup/init.sql;"
+
+sudo echo "wait_timeout=31536000" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo echo "interactive_timeout=31536000" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo /etc/init.d/mysql restart
